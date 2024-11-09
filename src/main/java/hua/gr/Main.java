96bacefacbd132f57772fe6f06a30131582d1d39
Renato Nake <nakerenato@gmail.com>
2024-11-09 16:08:42 +0200

    small fix

--- src/main/java/hua/gr/Main.java
+++ src/main/java/hua/gr/Main.java
@@ -3,29 +3,21 @@ package hua.gr;
 public class Main {
     public static void main(String[] args) {
 
-        if(args.length>0)
-        {
-            if (args[0]=="-list")
-            {
-                for (Integer recipes=1; recipes<=args.length-1;recipes++) //recipes=1 so we ignore the first "-list" arg
-                {
+        if (args.length > 0) {
+            if (args[0].equals("-list")) {
+                for (Integer recipes = 1; recipes <= args.length - 1; recipes++) { // recipes=1 so we ignore the first "-list" arg
                     System.out.println("Passed2");
                 }
-            }
-            else if (args[0]!="-list" && args.length>1){
+            } 
+            else if (!args[0].equals("-list") && args.length > 1) {
                 throw new IllegalArgumentException("No valid syntax, add -list for more than 1 recipes");
-            }
-            else{
+            } 
+            else {
                 System.out.println("Passed");
             }
-        }
-        else
-        {
+        } 
+        else {
             throw new IllegalArgumentException("You did not provide any recipe!");
         }
-
-
-
-    
-}
+    }
 }
--- src/main/java/hua/gr/RecipeParser.java
+++ src/main/java/hua/gr/RecipeParser.java
@@ -5,12 +5,8 @@ package hua.gr;
 
 
 public class RecipeParser {
-    for (int i =0; i<=args.length-1;i++)
-    {
-        System.out.println(args[i]);
-    }
-    public RecipeParser()
-    {
-        
-    }
+
+
+
+
 }
